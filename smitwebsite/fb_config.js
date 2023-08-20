2de4a8c61cb4d87a110c98c6b529d6bd57f7d1f6
NashraYousufZaii <123868324+NashraYousufZaii@users.noreply.github.com>
2023-08-20 13:04:28 +0500

    Add files via upload

---
+++ smitwebsite/fb_config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBZZ41Qy-QWPL33YD3z1CyMGAsLrBEpVJI",
+    authDomain: "smit-marketplace-42843.firebaseapp.com",
+    databaseURL: "https://smit-marketplace-42843-default-rtdb.firebaseio.com",
+    projectId: "smit-marketplace-42843",
+    storageBucket: "smit-marketplace-42843.appspot.com",
+    messagingSenderId: "809605667517",
+    appId: "1:809605667517:web:d5c5cec7d7d5a24df975d4",
+    measurementId: "G-0SGHLS2LTF"
+  };
